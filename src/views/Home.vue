17978c3e6f5c299514a41133ef8c21dbf053a289
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-26 09:25:08 -0300

    alteração no template corre adicionado modal.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
     <Navbar></Navbar>
+    <Hero />
     <produtos-lista></produtos-lista>
+    <div class="container h-100">
+        <div class="row p-5 h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <img src="/img/svg/product.svg" alt="" width="400">
+          </div>
+          <div class="col-md-6">
+            <h3>Learn More About us</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quaerat voluptate iusto? Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!
+            </p>
+             <button class="btn custom-btn btn-outline-primary mr-4">Browse Products</button>
+          </div>
+        </div>
+      </div>
     <login></login>
+
+    <mini-carrinho></mini-carrinho>
   </div>
 </template>
 
 <script>
 import Login from '@/components/Login.vue'
+import Hero from "@/components/Hero.vue";
 //import ProdutoLista from "@/sections/ProdutoLista";
 export default {
   name: 'home',
   components: {
-    Login
+    Hero, Login
   }
 }
 </script>
